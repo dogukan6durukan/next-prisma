142fbf629c5fa57824854026d52941b3926ca751
dokuzor <dokuzorofficial@gmail.com>
2022-12-25 00:34:30 +0300

    adapt the app to deployment

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,31 @@ import { Wrapper } from "../components/utils/Wrapper";
 import { SuggestionForm } from "../components/SuggestionForm";
 import { SuggestList } from "../components/SuggestList";
 import { Warning } from "../components/utils/Warning";
+import { PrismaClient } from "@prisma/client"
 
 
-export default function Home({ suggests }) {
+export default function Home({ allSuggests }) {
   
-  console.log(suggests);
+  console.log(allSuggests);
 
   return (
     <>
       <Wrapper>
         <SuggestionForm />
-        {/* {suggests.length > 0 ? <SuggestList suggests={suggests}/> : <Warning type="informative" text="There is no suggestion yet."/>} */}
+        {allSuggests.length > 0 ? <SuggestList suggests={allSuggests}/> : <Warning type="informative" text="There is no suggestion yet."/>}
       </Wrapper>
     </>
   );
 }
 
 export async function getStaticProps() {
-  const req = await fetch("http://localhost:3000/api/getsuggests");
-  const suggests = await req.json();
+  const prisma = new PrismaClient();
+  const allSuggests = await prisma.suggest.findMany();
+
 
   return {
     props : {
-      suggests
+      allSuggests
     },
     revalidate : 5
   }
