617a6c98a5f4c1fc13d4176782d7d2bc7baed17b
dokuzor <dokuzorofficial@gmail.com>
2022-12-25 10:55:11 +0300

    changed url to vercel url

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allSuggests }) {
 }
 
 const fetcher = async () => {
-  const data = await fetch('http://localhost:3000/api/getsuggests')
+  const data = await fetch('https://next-prisma-blond.vercel.app/api/getsuggests')
   const result = await data.json();
   return result;
 
