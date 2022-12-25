cd660ce09c586f63827731ceaebe224e61c01a62
dokuzor <dokuzorofficial@gmail.com>
2022-12-25 11:00:52 +0300

    change ui

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,31 @@ import { Wrapper } from "../components/utils/Wrapper";
 import { SuggestionForm } from "../components/SuggestionForm";
 import { SuggestList } from "../components/SuggestList";
 import { Warning } from "../components/utils/Warning";
-import useSWR from 'swr';
+import useSWR from "swr";
 
 export default function Home({ allSuggests }) {
-
-  const { data, error } = useSWR('/api/getsuggests', fetcher);
-
-  if(!data) return 'Loading...';
+  const { data, error } = useSWR("/api/getsuggests", fetcher);
 
   return (
     <>
       <Wrapper>
         <SuggestionForm />
-        {data.length > 0 ? <SuggestList suggests={data}/> : <Warning type="informative" text="There is no suggestion yet."/>}
+        {!data ? (
+          <p>Loading Suggests...</p>
+        ) : data.length > 0 ? (
+          <SuggestList suggests={data} />
+        ) : (
+          <Warning type="informative" text="There is no suggestion yet." />
+        )}
       </Wrapper>
     </>
   );
 }
 
 const fetcher = async () => {
-  const data = await fetch('https://next-prisma-blond.vercel.app/api/getsuggests')
+  const data = await fetch(
+    "https://next-prisma-blond.vercel.app/api/getsuggests"
+  );
   const result = await data.json();
   return result;
-
-}
+};
