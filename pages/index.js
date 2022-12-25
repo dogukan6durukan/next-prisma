d25ece819cee275c1056a0496fc98c96572b31b3
dokuzor <dokuzorofficial@gmail.com>
2022-12-25 11:21:03 +0300

    applied api

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home({ allSuggests }) {
 
 const fetcher = async () => {
   const data = await fetch(
-    "https://next-prisma-blond.vercel.app/"
+    "https://next-prisma-blond.vercel.app/api/getsuggests"
   );
   const result = await data.json();
   return result;
